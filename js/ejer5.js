86a845b633edd9b9c32273c9f5e7b8af6bee5096
KarlaJMT <2302091@utrivieramaya.edu.mx>
2024-11-21 07:48:47 -0500

    U3-Exam

---
+++ js/ejer5.js
@@ -0,0 +1,14 @@
+// Ejercicio 5:
+// Haz un formulrio que convierta de euros a dólares. Supondremos que un euro son 1,10 dólares y que un dólar son 0,91 euros.
+
+// let res = euro * 1.10
+// let res2 = dolar * 0.91
+let res = prompt("Escriba los dólares a transformar a euros: ")
+euro = res * 0.91
+console.log("Los dólares introducidos son ",euro, " euros.")
+
+let res2 = prompt("Escriba los euros a transformar a dólares: ")
+dolar = res2 * 1.10
+console.log("Los euros introducidos son ",dolar, " dólares.")
+
+// KARLA JAQUELINE MUÑOZ TREJO TI-31.
---
+++ js/ejer6.js
@@ -0,0 +1,8 @@
+// Ejercicio 6:
+// Haga un forumulario que convierta de grados Celsius a grados Farenheit. Para ello, tendré que multiplicar por 9/5 y sumar 32. Tenga en cuenta que 30 grados Celsius son 86 grados Farenheit.
+
+let res = prompt("Escriba los grados Celcius: ")
+far = res * (9/5) + 32
+console.log("Los grados Celsius introducidos son: ",far," grados Farenheit.")
+
+// KARLA JAQUELINE MUÑOZ TREJO TI-31.
